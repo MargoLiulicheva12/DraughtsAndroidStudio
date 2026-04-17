5e3419a3d8e42bb54298a63efa581f975926448c
Margo <Margo@LAPTOP-MARGO>
2026-04-17 14:09:11 +0300

    Перемещение класса перечислений в общий класс

--- app/src/main/java/com/example/newgame/Enums.kt
+++ app/src/main/java/com/example/newgame/Enums.kt
@@ -1,3 +1,19 @@
 package com.example.newgame
 
 enum class Difficulty { EASY, MEDIUM, HARD }
+
+// Перечисление всех возможных состояний клетки на доске
+enum class Piece {
+    EMPTY, WHITE, BLACK, WHITE_KING, BLACK_KING;
+
+    // Является ли фигура белой (простая или дамка)
+    fun isWhite() = this == WHITE || this == WHITE_KING
+    // Является ли фигура черной (простая или дамка)
+    fun isBlack() = this == BLACK || this == BLACK_KING
+    // Является ли фигура дамкой
+    fun isKing() = this == WHITE_KING || this == BLACK_KING
+    // Если фигура белая - возвращает черную, и наоборот
+    fun opponentColor(): Piece {
+        return if (this.isWhite()) BLACK else WHITE
+    }
+}
